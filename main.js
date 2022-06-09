da5fef48999363bcbe8e469bf8b1980c4b078c34
HenriqueMAF <104100108+HenriqueMAF@users.noreply.github.com>
2022-06-09 16:25:14 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,102 @@
+//Abre e fecha o menu
+const nav = document.querySelector('#header nav')
+const toggle = document.querySelectorAll('nav .toggle')
+
+for (const element of toggle) {
+  element.addEventListener('click', function () {
+    nav.classList.toggle('show')
+  })
+}
+
+//selecionar um item do menu
+const links = document.querySelectorAll('nav ul li a')
+
+for (const link of links) {
+  link.addEventListener('click', function () {
+    nav.classList.remove('show')
+  })
+}
+
+//mudar o heard da pagina quando der o scroll (evento criado no fim)
+const header = document.querySelector('#header')
+const navHeight = header.offsetHeight
+function changeHeaderWhenScroll() {
+  if (window.scrollY >= navHeight) {
+    header.classList.add('scroll')
+  } else {
+    header.classList.remove('scroll')
+  }
+}
+
+// SLIDER DOS DEPOIMENTOS SWEPIR
+const swiper = new Swiper('.swiper-container', {
+  slidesPerView: 1,
+  pagination: {
+    el: '.swiper-pagination'
+  },
+  mousewheel: true,
+  keyboard: true,
+  breakpoints: {
+    767: {
+      slidesPerView: 2,
+      setWrapperSize: true
+    }
+  }
+})
+
+//ScrollReval mostrar elementos quando rolar a pagina
+const scrollReveal = ScrollReveal({
+  origin: 'top',
+  distance: '30px',
+  duration: 900,
+  reset: true
+})
+scrollReveal.reveal(
+  `#home .image, #home .text,
+  #about .image, #about .text,
+  #sercices header, #services .card,
+  #testimonials header, #testimonials .testimonials,
+  #contact .text, #contact .links,
+  footer .brand, footer .social
+  `,
+  { interval: 100 }
+)
+
+// VOLTAR PARA O TOP
+const backToTopButton = document.querySelector('.back-to-top')
+function backToTop() {
+  if (window.scrollY >= 500) {
+    backToTopButton.classList.add('show')
+  } else {
+    backToTopButton.classList.remove('show')
+  }
+}
+//menu ativo de acordo com a seção da pagina
+const sections = document.querySelectorAll('main section[id]')
+function activateMenuAtCurrentSection() {
+  const checkpoint = window.pageYOffset + (window.innerHeight / 8) * 4
+  for (const section of sections) {
+    const sectionTop = section.offsetTop
+    const sectionHeight = section.offsetHeight
+    const sectionId = section.getAttribute('id')
+
+    const checkpointStart = checkpoint >= sectionTop
+    const checkpointEnd = checkpoint <= sectionTop + sectionHeight
+
+    if (checkpointStart && checkpointEnd) {
+      document
+        .querySelector('nav ul li a[href*=' + sectionId + ']')
+        .classList.add('active')
+    } else {
+      document
+        .querySelector('nav ul li a[href*=' + sectionId + ']')
+        .classList.remove('active')
+    }
+  }
+}
+//Evento de scroll (mudança de backgroud e voltar ao TOP)
+window.addEventListener('scroll', function () {
+  changeHeaderWhenScroll()
+  backToTop()
+  activateMenuAtCurrentSection()
+})
